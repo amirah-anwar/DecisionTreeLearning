050e28e46efcf355f7c3ce8a60e2cc8ce674fcc3
Michelle Becerra <michel8@vt.edu>
2017-09-11 11:15:35 -0700

    changes to data struc and data parsing

--- DecisionTree.py
+++ DecisionTree.py
@@ -4,6 +4,32 @@ import numpy as np
 #Calculate Entropy of each subset within an attribute
 #Calculate Information gain of each level or attribute
 
+def main():
+#==============Process Data==================================
+#Get rid of the first column, may not be needed
+	with open('dt-data.txt', 'r') as f:
+		filedata = f.read()
+	filedata = filedata.replace(':', ',')
+	filedata = filedata.replace(';', ', ')
+
+	with open('dt-data.txt', 'w') as f:
+		f.write(filedata)
+
+	#load txt file into a ndarray
+	data = np.loadtxt('dt-data.txt', dtype='string', comments='(', delimiter=', ', usecols=(1,2,3,4,5,6,7))
+	#classification column
+	targetClass = data[:,6]	
+
+	#Create Tuples of (feature, classification)
+	occupiedTup = zip(data[:,0], targetClass)
+	priceTup = zip(data[:,1], targetClass);
+	musicTup = zip(data[:,2], targetClass);
+	locationTup = zip(data[:,3], targetClass);
+	vipTup = zip(data[:,4], targetClass);
+	beerTup = zip(data[:,5], targetClass);
+
+	print priceTup
+
 #=============Method Definitions===========================
 def entropy(s):
 	sum = 0;
@@ -20,20 +46,5 @@ def informationGain(s, att):
 	branches, counts = np.unique(att, return_counts=True)
 	print branches, counts
 
-#==============Process Data==================================
-#Get rid of the first column, may not be needed
-with open('dt-data.txt', 'r') as f:
-	filedata = f.read()
-filedata = filedata.replace(':', ',')
-
-with open('dt-data.txt', 'w') as f:
-	f.write(filedata)
-
-#load txt file into a ndarray
-data = np.loadtxt('dt-data.txt', dtype='string', comments=('(',';'), delimiter=', ', usecols=(1,2,3,4,5,6))
-#classification column
-targetClass = data[:,5]
-
-print informationGain(targetClass, data[:,0])
-
-
+if __name__ == "__main__":
+    main()
